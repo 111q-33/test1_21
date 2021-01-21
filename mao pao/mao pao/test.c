fa465b44a74fa8a11e065177ee0a615285c083ed
111q-33 <2454924181@qq.com>
2021-01-21 17:42:49 +0800

    冒泡排序

---
+++ mao pao/mao pao/test.c
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+//int main()
+//{
+//	int n = 0;
+//	int score[40] = { 0 };
+//	scanf("%d", &n);
+//	int i = 0;
+//	for (i = 0; i<n; i++)
+//	{
+//		scanf("%d", &score[i]);
+//	}
+//	//对所有数字排序-冒泡排序
+//	int j = 0;
+//	for (i = 0; i<n; i++)
+//	{
+//		for (j = 0; j<n - i - 1; j++)
+//		{
+//			if (score[j] < score[j + 1])
+//			{
+//				int tmp = score[j];
+//				score[j] = score[j + 1];
+//				score[j + 1] = tmp;
+//			}
+//		}
+//	}
+//
+//	for (i = 0; i<5; i++)
+//	{
+//		printf("%d ", score[i]);
+//	}
+//
+//	return 0;
+//}
+int main()
+{
+	char a = 3;
+	char b = 252;
+	printf("%d", a + b);
+	return 0;
+}
+//整形提升
